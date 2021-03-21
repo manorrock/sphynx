dddea949b0a2df8ac85d8ca18ad7cbe01c3d7431
Manfred Riem <22894110+manorrock@users.noreply.github.com>
2021-03-21 14:33:36 -0600

    Fixes issue #106 - Add ability to set XY on LCB001 light

---
+++ hue/lcb001/src/test/java/com/manorrock/sphynx/hue/lcb001/HueLCB001LightTest.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2002-2021 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its 
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.sphynx.hue.lcb001;
+
+import com.manorrock.sphynx.hue.bridge.DefaultHueBridge;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * The JUnit test for the HueLCB001Light class.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class HueLCB001LightTest {
+    
+    /**
+     * Test changeBrightness method.
+     */
+    @Test
+    public void testChangeBrightness() {
+        if (!System.getProperty("sphynx.hue.lcb001.id").equals("")) {
+            DefaultHueBridge bridge = new DefaultHueBridge();
+            bridge.setBaseUrl(System.getProperty("sphynx.hue.bridge.baseUrl"));
+            bridge.setUsername(System.getProperty("sphynx.hue.bridge.username"));
+            HueLCB001Light light = new HueLCB001Light();
+            light.setId(Integer.valueOf(System.getProperty("sphynx.hue.lcb001.id")));
+            light.setBridge(bridge);
+            light.on();
+            light.changeBrightness(50);
+            int brightness = light.getBrightness();
+            light.off();
+            assertEquals(50, brightness);
+        }
+    }
+    
+    /**
+     * Test changeXY method.
+     */
+    @Test
+    public void testChangeXY() {
+        if (!System.getProperty("sphynx.hue.lcb001.id").equals("")) {
+            DefaultHueBridge bridge = new DefaultHueBridge();
+            bridge.setBaseUrl(System.getProperty("sphynx.hue.bridge.baseUrl"));
+            bridge.setUsername(System.getProperty("sphynx.hue.bridge.username"));
+            HueLCB001Light light = new HueLCB001Light();
+            light.setId(Integer.valueOf(System.getProperty("sphynx.hue.lcb001.id")));
+            light.setBridge(bridge);
+            light.on();
+            light.changeXY(new float[] { 0.45f, 0.40f });
+            float[] xy = light.getXy();
+            light.off();
+            assertEquals(0.45f, xy[0], 0.1);
+            assertEquals(0.40f, xy[1], 0.1);
+        }
+    }
+    
+    /**
+     * Test getId method.
+     */
+    @Test
+    public void testGetId() {
+        if (!System.getProperty("sphynx.hue.lcb001.id").equals("")) {
+            HueLCB001Light light = new HueLCB001Light();
+            light.setId(Integer.valueOf(System.getProperty("sphynx.hue.lcb001.id")));
+            assertEquals((int) Integer.valueOf(System.getProperty("sphynx.hue.lcb001.id")), light.getId());
+        }
+    }
+    
+    /**
+     * Test getBridge method.
+     */
+    @Test
+    public void testGetBridge() {
+        if (!System.getProperty("sphynx.hue.lcb001.id").equals("")) {
+            DefaultHueBridge bridge = new DefaultHueBridge();
+            HueLCB001Light light = new HueLCB001Light();
+            light.setBridge(bridge);
+            assertEquals(bridge, light.getBridge());
+        }
+    }
+    
+    /**
+     * Test on method.
+     */
+    @Test
+    public void testOn() {
+        if (!System.getProperty("sphynx.hue.lcb001.id").equals("")) {
+            DefaultHueBridge bridge = new DefaultHueBridge();
+            bridge.setBaseUrl(System.getProperty("sphynx.hue.bridge.baseUrl"));
+            bridge.setUsername(System.getProperty("sphynx.hue.bridge.username"));
+            HueLCB001Light light = new HueLCB001Light();
+            light.setBridge(bridge);
+            light.setId(Integer.valueOf(System.getProperty("sphynx.hue.lcb001.id")));
+            light.on();
+        }
+    }
+    
+    /**
+     * Test off method.
+     */
+    @Test
+    public void testOff() {
+        if (!System.getProperty("sphynx.hue.lcb001.id").equals("")) {
+            DefaultHueBridge bridge = new DefaultHueBridge();
+            bridge.setBaseUrl(System.getProperty("sphynx.hue.bridge.baseUrl"));
+            bridge.setUsername(System.getProperty("sphynx.hue.bridge.username"));
+            HueLCB001Light light = new HueLCB001Light();
+            light.setBridge(bridge);
+            light.setId(Integer.valueOf(System.getProperty("sphynx.hue.lcb001.id")));
+            light.off();
+        }
+    }
+}
